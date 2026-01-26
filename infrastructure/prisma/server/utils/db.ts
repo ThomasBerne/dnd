73a09cefee53c2e890145670570b1738626cdc8a
ThomasBerneHCSE <thomas.berne@helloce.fr>
2026-01-26 09:40:49 +0100

    qsd

--- infrastructure/prisma/server/utils/db.ts
+++ infrastructure/prisma/server/utils/db.ts
@@ -1,17 +1,17 @@
-import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../../generated/prisma/client';
+// import { PrismaPg } from '@prisma/adapter-pg';
+// import { PrismaClient } from '../../generated/prisma/client';
 
-const prismaClientSingleton = (): PrismaClient => {
-  const pool = new PrismaPg({
-    connectionString: `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
-  });
-  return new PrismaClient({ adapter: pool });
-};
+// const prismaClientSingleton = (): PrismaClient => {
+//   const pool = new PrismaPg({
+//     connectionString: `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+//   });
+//   return new PrismaClient({ adapter: pool });
+// };
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
 
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+// export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
