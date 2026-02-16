ff887fad9bafcb3952feb9a5527db2d12e35eb10
ThomasBerneHCSE <thomas.berne@helloce.fr>
2026-02-16 11:33:10 +0100

    chore(deps): bump deps

--- infrastructure/vite/nuxt.config.ts
+++ infrastructure/vite/nuxt.config.ts
@@ -1,16 +1,7 @@
 export default defineNuxtConfig({
   vite: {
     optimizeDeps: {
-      include: [
-        '@vue/devtools-core',
-        '@vue/devtools-kit',
-        '@nuxt/hints/runtime/hydration/component',
-        'dompurify',
-        '@vueuse/core',
-        'html-to-image',
-        '@nuxt/ui/utils/editor',
-        '@tiptap/extension-table',
-      ],
+      noDiscovery: true,
     },
   },
 });
